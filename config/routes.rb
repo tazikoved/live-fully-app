dd5775a04805fbac583ece803911a2866dbce41c
Denis Gavrilenco <denis.gavrilenco@gmail.com>
2022-07-01 16:30:08 +0200

    created routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  root to: 'pages#home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :experiences do
+    resources :reviews, only: [:create]
+    resources :bookings, only: [ :new, :create, :update ]
+  end
+
+  get 'dashboard', to: 'pages#dashboard'
+
 end
